eddd768bd621a7161cd749275591a36c8ffe43a8
Kevin Klein <kev.m.klein@gmail.com>
2026-04-30 10:41:01 +0800

    wip: basic user upload functionality for web app front/back

--- webapp/client/src/App.jsx
+++ webapp/client/src/App.jsx
@@ -1,3 +1,88 @@
-export default function App() {
-  return <h1>I work!</h1>;
+/**
+ * Citations:
+ * https://www.geeksforgeeks.org/reactjs/file-uploading-in-react-js/
+ */
+import axios from "axios";
+import React, { useState } from "react";
+import cors from "cors"
+
+const MAX_CONTENT_SIZE = 10 * 1024 * 1024  // 10MB 
+
+async function uploadFile(file) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const response = await fetch("http://localhost:5137/api/predict", {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    const text = await response.text();
+    console.error("Server error:", text);
+    return;
+  }
+
+  const data = await response.json();
+  console.log(data);
 }
+
+
+function bytes_to_MB(bytes) {
+  return (bytes / (1024 * 1024)).toFixed(2);
+}
+
+
+const App = () => {
+	const [selectedFile, setSelectedFile] = useState(null);
+
+	const onFileChange = (event) => {
+		setSelectedFile(event.target.files[0]);
+	};
+
+  const onFileUpload = () => {
+    if (!selectedFile) return;
+    if (selectedFile.size > MAX_CONTENT_SIZE) {
+      alert("File too large");
+      console.log(formatSize(file.size));
+      return;
+    }
+    uploadFile(selectedFile);
+  };
+
+	const fileData = () => {
+		if (!selectedFile) return;
+    uploadFile(selectedFile)
+    if (selectedFile.size > MAX_CONTENT_SIZE) {
+      return (
+        <div>
+          <h2>File size {bytes_to_MB(selectedFile.size)}MB exceeds maximum (10MB)</h2>
+          <h3>Please select smaller file.</h3>
+          <p>File Name: {selectedFile.name}</p>
+          <p>File Type: {selectedFile.type}</p>
+        </div>
+      )
+    }
+    return (
+      <div>
+        <h2>File Details:</h2>
+        <p>File Name: {selectedFile.name}</p>
+        <p>File Type: {selectedFile.type}</p>
+      </div>
+    );
+  }
+
+	return (
+		<div>
+			<h1>WaveRank</h1>
+			<h3>Choose a file to upload</h3>
+			<div>
+				<input type="file" onChange={onFileChange} />
+				<button onClick={onFileUpload}>Upload!</button>
+			</div>
+			{fileData()}
+		</div>
+	);
+};
+
+export default App;
--- webapp/server/app.py
+++ webapp/server/app.py
@@ -1,10 +1,65 @@
-from flask import Flask
+"""
+Citation (4/30):
+https://flask.palletsprojects.com/en/stable/patterns/fileuploads/
+"""
+from flask import Flask, request, jsonify
+import os
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+
+
+PORT = 5137
+UPLOAD_FOLDER = "../uploads"
+ALLOWED_EXTENSIONS = {'wav', 'mp3', 'mp4'}
+MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10MB 
+
 
 app = Flask(__name__)
+CORS(app)
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# ----- ROUTES -----
 @app.route('/')
 def main():
     return "I work!"
 
+@app.route("/api/predict", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        flash('No file part')
+        return jsonify({"error": "File to upload doesn't exist"}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('No selected file')
+        return jsonify({"error": "No file selected"}), 400
+
+    if not allowed_file(file.filename):
+        return jsonify({"error": "Invalid file type"}), 400
+
+    filename = secure_filename(file.filename)  # deal with dangerous filenames
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+    file.save(filepath)
+    # Convert to .wav
+    # Send to inference pipeline
+    # Receive stuff from inference pipeline
+
+    return jsonify({
+        "message": "File uploaded",
+        "filename": file.filename
+        # and more stuff from inference pipeline
+    })
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=PORT)
