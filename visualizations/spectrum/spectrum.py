78354cb2334b8515a05b98b7a052238b2b1f2387
Emily Huntley <emilyfhuntley@gmail.com>
2026-05-21 23:09:29 -0600

    Updated my branch to match Kevin's changes to main

--- visualizations/spectrum/spectrum.py
+++ visualizations/spectrum/spectrum.py
@@ -5,42 +5,30 @@ This visualization is tailored for human viewing, not CNN input.
 Citation (5/14):
 https://medium.com/analytics-vidhya/understanding-the-mel-spectrogram-fca2afa2ce53
 """
-from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
-from shared.audio_utils import load_audio
 from visualizations.config import GRAPH_COLOR
 
 
-def generate_spectrum(filepath, output_dir):
+def generate_spectrum(y, output_path):
     """
-    Generate a spectrum graph of the audio file at the given path.
+    Generate a spectrum graph of the given audio time series.
     Args:
-        filepath (PosixPath): path of audio file
-        output_dir (PosixPath): path of directory to save graph into
-    Returns:
-        output_filename (str): name of created graph
+        y (np.ndarray): audio time series
+        output_dir (Path): path of directory to save graph into
     Side Effects:
         Graph saved to disk at output_path
     """
-    output_filename = filepath.stem + "_spectrum.png"
-    output_path = output_dir / output_filename
-
-    y, _ = load_audio(filepath)
-    
-    # compute spectrum
+    # Compute spectrum
     windowed = y * np.hanning(len(y))
     spectrum = np.abs(np.fft.rfft(windowed))
 
-    # plot graph
-    plt.figure()
-    plt.clf()
-    plt.plot(spectrum, color=GRAPH_COLOR)
-    plt.title('Spectrum')
-    plt.xlabel('Frequency Bin')
-    plt.ylabel('Amplitude')
+    # Plot graph
+    fig, ax = plt.subplots()
+    ax.plot(spectrum, color=GRAPH_COLOR)
+    ax.set_title('Spectrum')
+    ax.set_xlabel('Frequency Bin')
+    ax.set_ylabel('Amplitude')
 
-    plt.savefig(output_path)
-    plt.close()
-
-    return output_filename
+    fig.savefig(output_path)
+    plt.close(fig)
